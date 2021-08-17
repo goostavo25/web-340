c4e0e847b94bd925d7d789c18cb7ee374510dc3b
goostavo25 <groogonzalez@my365.bellevue.edu>
2021-08-17 14:11:14 -0400

    Added comments

--- week-1/roogonzalez-nodejs-in-action.js
+++ week-1/roogonzalez-nodejs-in-action.js
@@ -5,16 +5,20 @@ Date: 15 August 2021
 Description: Assignment 1.5 Hello World
 */
 
+// Including the HTTP module
 var http = require("http");
 
+// Function that responds to request from client
 function processRequest(req, res) {
     var body = "This is Gustavo's web server, to infinity ...and beyond!";
     var contentLength = body.length;
-    res.writeHead(200, {
+    res.writeHead(200, { //write a response to the client
         "Content-Length": contentLength,
         "Content-Type": "text/plain",
     });
-    res.end(body);
+    res.end(body); //end of the response
 }
+
+// Opens webserver object that listens on port 8080
 var s = http.createServer(processRequest);
 s.listen(8080);
